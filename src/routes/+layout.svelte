9fd87941abf333ac77aea2bf6b9aa2e850144b20
suhailkk10 <52050504+suhailkk10@users.noreply.github.com>
2026-04-03 15:27:41 +0530

    email logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -307,9 +307,7 @@
 							</div>
 						</li>
 						<li class="flex items-center gap-3">
-							<svg class="h-5 w-5 shrink-0 text-homigo-green-light" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-							</svg>
+							<img src="/assets/email_logo.svg" alt="" class="h-5 w-5 shrink-0" />
 							<span>info@homigotechnicalservices.com</span>
 						</li>
 					</ul>
